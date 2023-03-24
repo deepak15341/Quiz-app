1b8115e4e9fa6423eeee2f20b9a2cddc042f3686
deeoak15341 <deepakkumarsahbgp123@gmail.com>
2023-03-24 14:58:46 +0530

    1.0

--- app/src/main/java/com/deepak15341/quizapp/MainActivity.java
+++ app/src/main/java/com/deepak15341/quizapp/MainActivity.java
@@ -1,14 +1,101 @@
 package com.deepak15341.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  QuizModel[] questionsAll =new QuizModel[]{
+            new QuizModel(R.string.q1,true),
+            new QuizModel(R.string.q2,true),
+            new QuizModel(R.string.q3,false),
+            new QuizModel(R.string.q4,true),
+            new QuizModel(R.string.q5,true),
+            new QuizModel(R.string.q6,true),
+            new QuizModel(R.string.q7,false),
+            new QuizModel(R.string.q8,true),
+            new QuizModel(R.string.q9,true),
+            new QuizModel(R.string.q10,true)
+    };
+
+    private int mQuestionindex,mquizQuestion,result=0;
+    final int USER_PROGRESS = (int) Math.ceil(100.0/questionsAll.length);
+    private AppCompatButton buttonCorrect, buttonWrong;
+    private TextView textViewQuestion,textViewQuizStats;
+    private ProgressBar quizProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        buttonCorrect = findViewById(R.id.correctButton);
+        buttonWrong  =findViewById(R.id.wrongButton);
+        textViewQuestion =findViewById(R.id.textquestion);
+        textViewQuizStats = findViewById(R.id.textquizStats);
+        quizProgressBar =findViewById(R.id.quizProgressBar);
+
+        //get question for the first element
+        textViewQuestion.setText(questionsAll[mquizQuestion].getmQuestion());
+
+       // textViewQuizStats.setText((mQuestionindex+1)+"/"+questionsAll.length);
+        textViewQuizStats.setText(result+"/"+10);
+
+
+
+
+       buttonCorrect.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+                evaluateAnswer(true);
+               questionChange();
+
+           }
+       });
+
+       buttonWrong.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               evaluateAnswer(false);
+               questionChange();
+           }
+       });
+
+
     }
+
+    private void questionChange(){
+
+            quizProgressBar.incrementProgressBy(USER_PROGRESS);
+            mQuestionindex = mQuestionindex + 1;
+            if(mQuestionindex<questionsAll.length) {
+                mquizQuestion = questionsAll[mQuestionindex].getmQuestion();
+                textViewQuestion.setText(mquizQuestion);
+
+                //textViewQuizStats.setText((mQuestionindex+1)+"/"+questionsAll.length);
+            }
+
+    }
+
+    private void evaluateAnswer(boolean userGuess) {
+
+        if (mQuestionindex < questionsAll.length) {
+            boolean currentQuestionAnswer = questionsAll[mQuestionindex].ismAnswer();
+            if (currentQuestionAnswer == userGuess) {
+                Toast.makeText(getApplicationContext(), R.string.correct_toast_message, Toast.LENGTH_SHORT).show();
+                result++;
+                textViewQuizStats.setText(result+"/"+10);
+            } else {
+                Toast.makeText(getApplicationContext(), R.string.incorrect_toast_message, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
